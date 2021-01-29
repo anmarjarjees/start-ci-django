949a148d3b5082800da7dd093688092126c00dec
Anmar Jarjees <aj@anmarjarjees.com>
2021-01-29 22:09:58 +0000

    adding our model-class part1

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,28 @@
+packages
+# here is the link for Django github: https://github.com/django/django
+# then you can follow this path: django/django/db/models/__init__.py
+# inside the file __init__.py, you will see this line:
+# from django.db.models.base import DEFERRED, Model  # isort:skip
+
 from django.db import models
 
 # Create your models here.
+
+# Creating our class "Item"
+# Django will convert the "Item" class to "Item" tables in the database!
+# Any custom class we add to python, we have to use class inhertince to inhirit from python models as explained above
+# below we are inherting from Django the base models.Model class:
+# this is the basic concept of OOP [Please refer to my lecture about OOP in Python for more details]
+class Iten(models.Model):
+    # we need to define the attributes (the fields):
+    # plus adding some restrictions for each field
+
+    # name field:
+    # maximum 50 characters
+    # null=False => to prevent created/inserting a new item without a name programmatically (the backend - srouce code)
+    # blank=False => will make the field required on forms (the view)
+    # With these two constraints, we can be sure the this field will never be empty by the user form or the python code or in the admin panel
+    name = models.CharField(max_length=50, null=False, blank=False)
+
+    # adding the same resitrictions for done field below and adding a defualt value of False:
+    done = models.BooleanField(null=False, blank=False, default=False) 
